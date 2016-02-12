147ce91ac78b28f15c7ba58bfc6353f3b9634722
Tom Kirby <rtkirby@mac.com>
2016-02-12 14:28:30 -0700

    Cont Menu Suite
    Test to validate function of icon clicked toggle

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,19 +44,27 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    //The menu test suite
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        // Test ensures the menu "element" is hidden by default.
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        // This test validates proper functioning of the var menu toggle.
+        it('changes visibility when icon clicked', function() {
+            var clickedIcon = $('.menu-icon-link');
+
+            // This tests for menu display.
+            clickedIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            // This tests for menu hide.
+            clickedIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
